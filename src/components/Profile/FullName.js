c3dc3e09cc1ebf76f7b3341d30c2ce2e677acf9b
syndanouri <sinda.nouri@esprit.tn>
2023-02-24 12:01:52 +0100

    first commit

--- src/components/Profile/FullName.js
+++ src/components/Profile/FullName.js
@@ -1,12 +1,14 @@
 const FullName = () => {
   return (
     <div className="fname">
-     <h1 style={{color:"#00cba9"}}> Nouri sinda </h1> 
-     <h7> I'am a skilled and experienced web developer in FullStack JS using technologies such as Node.js, React,
-       express,and MongoDB to build powerful and responsive web applications</h7>
-    
+      <h1 style={{ color: "#00cba9" }}> Nouri sinda </h1>
+      <h7>
+        {" "}
+        Developing and implementing highly responsive user interface components
+        using React,Following application requirements and interface designs.
+      </h7>
     </div>
-  )
+  );
 }
 
 export default FullName
